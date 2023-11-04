b1944cc79bd4a59b86c3ff969dddeebefbdb0b5e
Kunal Tankra <kunaltankra2002@gmail.com>
2023-11-04 17:32:00 +0530

    setup redux

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Input from './components/input/Input';
 import ProgressBar from './components/progressBar/ProgressBar';
 
 function App() {
+
   return (
     <>
       <ProgressBar />
       <Alert/>
-
       <div className='container pt-5'>
         <h2 className='mb-5 text-center'>Zip Code Info App</h2>
 
